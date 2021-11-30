297ea0bbb296abeb02461ee8fcff19a0209f8e49
cgargatagli <ciroshaila@gmail.com>
2021-11-30 00:50:04 -0300

    Se agrega seleccionar día y reservar

--- BarApp/app/src/main/java/com/example/barapp/adapter/ItemAdapter.kt
+++ BarApp/app/src/main/java/com/example/barapp/adapter/ItemAdapter.kt
@@ -22,14 +22,20 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.graphics.Color
 import androidx.core.app.NotificationCompat;
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import com.facebook.FacebookSdk.getApplicationContext
 import android.app.PendingIntent
 import android.content.*
 import android.content.Intent
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.FileProvider
+import com.example.barapp.DatePickerFragment
+import com.example.barapp.MasterActivity
+import com.example.barapp.entity.Reserva
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.util.*
 
 
 class ItemAdapter(
@@ -51,26 +57,28 @@ class ItemAdapter(
         val ratingBar : RatingBar = view.findViewById(R.id.ratingBar)
         val textRating : TextView = view.findViewById(R.id.item_rating)
         val linePhone : View = view.findViewById(R.id.line_phone)
+        val editTextDate : EditText = view.findViewById(R.id.et_date)
+        val btnReservar : Button = view.findViewById(R.id.btnReservar)
         val facebook : ImageView = view.findViewById(R.id.iconFacebook)
         val instagram : ImageView = view.findViewById(R.id.iconInstagram)
     }
 
-    /**
-     * Create new views (invoked by the layout manager)
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
-    /**
-     * Replace the contents of a view (invoked by the layout manager)
-     */
-    @RequiresApi(Build.VERSION_CODES.R)
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
+
+        val auth = FirebaseAuth.getInstance()
+        val dbReferenceBar = FirebaseDatabase.getInstance().reference.child("Bar").child(item.id.toString()).child("Agenda")
+        val dbReferenceUsuario = FirebaseDatabase.getInstance().reference.child("Usuario").child(auth.currentUser?.uid.toString()).child("Reservas")
+        var dbReferenceDateSelected : DatabaseReference = dbReferenceBar
+        var dateSelected : String? = null
+
+
         Picasso.get()
             .load(item.img)
             .into(holder.imageView)
@@ -199,6 +207,77 @@ class ItemAdapter(
         holder.linePhone.setOnClickListener{
             val i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+" + item.telefono))
             startActivity(it.context, i, Bundle())
+        }
+
+        holder.editTextDate.setOnClickListener{
+            val datePicker = DatePickerFragment{ day, month, year ->
+                dateSelected = day.toString() + "-" + (month + 1).toString() + "-" + year.toString()
+                holder.editTextDate.setText(dateSelected)
+                holder.editTextDate.error = null
+
+                dbReferenceDateSelected = dbReferenceBar.child(dateSelected!!)
+                dbReferenceDateSelected.get().addOnSuccessListener {
+                        if (it.value == null){
+                            holder.btnReservar.isEnabled = true
+                        }else{
+                            dbReferenceDateSelected.child("MesasOcupadas").get().addOnSuccessListener {
+                                var mesasOcupadas = it.value.toString().toInt()
+
+                                if(mesasOcupadas >= item.capacidad){
+                                    holder.editTextDate.error = "${item.nombre} no tiene mesas disponibles el $dateSelected"
+                                    Toast.makeText(getApplicationContext(), "${item.nombre} no tiene mesas disponibles el $dateSelected", Toast.LENGTH_SHORT).show()
+                                    holder.btnReservar.isEnabled = false
+                                }
+                            }
+                        }
+                    }
+                dbReferenceUsuario.child(dateSelected + item.nombre).get().addOnSuccessListener{
+                    if (it.value == null){
+                        holder.btnReservar.isEnabled = true
+                    } else{
+                        holder.editTextDate.error = "Ya tenés una reserva en ${item.nombre} el $dateSelected"
+                        Toast.makeText(getApplicationContext(), "Ya tenés una reserva en ${item.nombre} el $dateSelected", Toast.LENGTH_SHORT).show()
+                        holder.btnReservar.isEnabled = false
+                    }
+                }
+             }
+            val activity = it.context as MasterActivity
+            datePicker.show(activity.supportFragmentManager, "datePicker")
+        }
+
+        holder.btnReservar.setOnClickListener {
+            dbReferenceDateSelected.get().addOnSuccessListener {
+                if (it.value == null){
+                    dbReferenceDateSelected.child("MesasOcupadas").setValue(1)
+                }else{
+                    dbReferenceDateSelected.child("MesasOcupadas").get().addOnSuccessListener {
+                        val increment = it.value.toString().toInt() + 1
+                        dbReferenceDateSelected.child("MesasOcupadas").setValue(increment)
+                    }
+                }
+            }
+            val reserva = Reserva(dateSelected, item.nombre,item.direccion, item.img, item.telefono, item.facebook, item.instagram.toString())
+            dbReferenceUsuario.child(dateSelected + item.nombre).setValue(reserva)
+
+            holder.editTextDate.setText("")
+            holder.btnReservar.isEnabled = false
+
+            val view = View.inflate(getApplicationContext(), R.layout.dialog_view, null)
+            val builder = AlertDialog.Builder(holder.btnReservar.context)
+            builder.setView(view)
+            val dialog = builder.create()
+            dialog.show()
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+            dialog.setCancelable(false)
+
+            view.findViewById<TextView>(R.id.text_dialog).text =
+                getApplicationContext().getString(R.string.dialog_text1) + " " + item.nombre + " " + getApplicationContext().getString(R.string.dialog_text2) + " " +  dateSelected
+
+            val btnConfirmar = view.findViewById<Button>(R.id.btn_confirm)
+
+            btnConfirmar.setOnClickListener {
+                dialog.dismiss()
+            }
         }
 
         holder.textCapacity.text = item.capacidad.toString()
---
+++ BarApp/app/src/main/java/com/example/barapp/entity/Reserva.kt
@@ -0,0 +1,29 @@
+package com.example.barapp.entity
+
+import java.util.*
+
+class Reserva(fecha : String?,
+              nombreBar : String?,
+              direccionBar : String?,
+              imgBar : String?,
+              telefonoBar : String?,
+              facebook : String?,
+              instagram : String ) {
+    var fecha: String? = null
+    var nombreBar: String? = ""
+    var direccionBar: String? = ""
+    var imgBar: String? = ""
+    var telefonoBar: String? = ""
+    var facebook: String? = ""
+    var instagram: String? = ""
+
+    init {
+        this.fecha = fecha
+        this.nombreBar = nombreBar
+        this.direccionBar = direccionBar
+        this.imgBar = imgBar
+        this.telefonoBar = telefonoBar
+        this.facebook = facebook
+        this.instagram = instagram
+    }
+}
